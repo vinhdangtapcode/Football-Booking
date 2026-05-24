e83692ec5483db393e1c73bd2870e38d152624df
Vinh <dovinhhp102@gmail.com>
2026-05-25 00:57:15 +0700

    Remove football-booking to merge history

--- football-booking/src/main/java/vn/footballfield/exception/InvalidBookingTimeException.java
+++
@@ -1,7 +0,0 @@
-package vn.footballfield.exception;
-
-public class InvalidBookingTimeException extends RuntimeException {
-	public InvalidBookingTimeException(String message) {
-		super(message);
-	}
-}
